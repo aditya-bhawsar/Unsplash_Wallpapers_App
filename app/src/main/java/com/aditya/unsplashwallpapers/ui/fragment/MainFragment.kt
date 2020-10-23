1d683efad4e05f571e902dc50c57240228dfda86
aditya-bhawsar <aditya.bhawsar98@gmail.com>
2020-10-23 17:03:07 +0530

    Adding Views in Fragment

--- app/src/main/java/com/aditya/unsplashwallpapers/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/aditya/unsplashwallpapers/ui/fragment/MainFragment.kt
@@ -1,8 +1,16 @@
 package com.aditya.unsplashwallpapers.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.aditya.unsplashwallpapers.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainFragment :Fragment(){
+class MainFragment :Fragment(R.layout.main_fragment){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
--- app/src/main/java/com/aditya/unsplashwallpapers/ui/fragment/SavedFragment.kt
+++ app/src/main/java/com/aditya/unsplashwallpapers/ui/fragment/SavedFragment.kt
@@ -1,8 +1,14 @@
 package com.aditya.unsplashwallpapers.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.aditya.unsplashwallpapers.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SavedFragment: Fragment() {
+class SavedFragment: Fragment(R.layout.saved_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }

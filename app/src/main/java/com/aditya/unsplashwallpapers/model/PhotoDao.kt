aaa17e09a8107a30582b2178c0e8e300ca1fdf97
aditya-bhawsar <aditya.bhawsar98@gmail.com>
2020-11-05 18:00:26 +0530

    Finished Wallpaper Showing

--- app/src/main/java/com/aditya/unsplashwallpapers/model/PhotoDao.kt
+++ app/src/main/java/com/aditya/unsplashwallpapers/model/PhotoDao.kt
@@ -5,14 +5,12 @@ import androidx.room.*
 
 @Dao
 interface PhotoDao {
-
     @Insert (onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPic(photoEntity: PhotoEntity)
     @Delete
     suspend fun deletePic(photoEntity: PhotoEntity)
     @Query("SELECT * FROM photo_tb WHERE id =(:idSent)")
     fun getPic(idSent:String):LiveData<PhotoEntity?>
-
     @Query("SELECT * FROM photo_tb")
     fun getAllPics(): LiveData<List<PhotoEntity>>
 }
